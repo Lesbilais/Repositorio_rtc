29f715a3f476ccff15c496bac7aa870b6ff25abc
Lesbilais <vicky.rubio@gmail.com>
2024-09-08 21:22:08 +0200

    Create profeleame.js

---
+++ Proyecto2/profeleame.js
@@ -0,0 +1,14 @@
+/*
+Este mensaje es para pedir disculpas por el retraso en la entrega de este proyecto.
+No quiero poner excusas, porque todo el mundo tiene problemas, pero este año estuve con una depresión exógena, ya que 
+perdí mi empleo y mi madre y mi suegro sufrieron cánceres. Mi madre cáncer al colón y mi suegro cáncer de esófago.
+Soy migrante y no pude ir a ver a mi madre por culpa de la falta de dinero. Por suerte, mi madre y 
+mi suegro ya se encuentran mucho mejor y lograron vencer los cánceres.
+He estado con ayuda psicológica y gracias a esta he podido retomar el estudio, y darme cuenta de mi depresión también.
+Desde ahora estoy mucho más metida en el estudio y espero seguir así. 
+Un saludo y si llegaste hasta aquí, muchas gracias por leerme.
+Victoria 
+
+
+
+*/
